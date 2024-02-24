78a6a82f2cc5ee964a0f2736a29f1956d1b58d25
Daisy <92063434+Anderanged@users.noreply.github.com>
2024-02-24 13:45:45 -0700

    added native jetpack class

--- MM_Bullfrog/cfgVehicles.hpp
+++ MM_Bullfrog/cfgVehicles.hpp
@@ -1,4 +1,21 @@
 class cfgVehicles {
+	class Bag_Base;
+	class MM_Bullfrog_Base : Bag_Base {
+		author = "Daisy";
+		model = "\A3\weapons_f\Ammoboxes\bags\Backpack_Compact";
+		displayName="[MMB] S-12 SOLA (Base)";
+		picture = "\A3\weapons_f\ammoboxes\bags\data\ui\icon_B_C_Compact_dgtl_ca.paa";
+		hiddenSelectionsTextures[] = { "\A3\weapons_f\ammoboxes\bags\data\backpack_compact_digi_co.paa" };
+		scope=1;
+		scopeArsenal=1;
+		maximumLoad=300;
+		mass=60;
+		isJetpack=1;
+		shortType="omni";
+		longType="omni";
+		maxCharge=100;
+	};
+
 	class OPTRE_S12_SOLA_Jetpack;
 	class MM_S12_SOLA_Omni: OPTRE_S12_SOLA_Jetpack {
 		scope=2;
@@ -10,25 +27,17 @@ class cfgVehicles {
 		shortType="omni";
 		longType="omni";
 		maxCharge=100;
-		shortCost=25;
-		longCost=50;
-		rechargeTime=1;
-		rechargeRate=2;
 	};
 	class MM_S12_SOLA_Vector: OPTRE_S12_SOLA_Jetpack {
 		scope=2;
 		scopeArsenal=2;
-		displayName="[MM] S-12 SOLA (Vector)";
+		displayName="[MM] S-12 SOLA (Vector)"; // suggest name changes to distinguish visually in-arsenal besides the identifiers in ()
 		maximumLoad=300;
 		mass=60;
 		isJetpack=1;
 		shortType="vector";
 		longType="vector";
 		maxCharge=100;
-		shortCost=25;
-		longCost=50;
-		rechargeTime=1;
-		rechargeRate=2;
 	};
 	class MM_S12_SOLA_VO: OPTRE_S12_SOLA_Jetpack {
 		scope=2;
@@ -40,10 +49,6 @@ class cfgVehicles {
 		shortType="vector";
 		longType="omni";
 		maxCharge=100;
-		shortCost=25;
-		longCost=50;
-		rechargeTime=1;
-		rechargeRate=2;
 	};
 	class MM_S12_SOLA_OV: OPTRE_S12_SOLA_Jetpack {
 		scope=2;
@@ -55,9 +60,5 @@ class cfgVehicles {
 		shortType="omni";
 		longType="vector";
 		maxCharge=100;
-		shortCost=25;
-		longCost=50;
-		rechargeTime=1;
-		rechargeRate=2;
 	};
 };
